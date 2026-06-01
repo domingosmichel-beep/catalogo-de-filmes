51a141b3efe2c44293d91b07f3d5a357177f01ef
michel luis  pioli <domingos.michel@escola.pr.gov.br>
2026-06-01 11:17:28 -0300

    Add files via upload

---
+++ catalagofilmes/script/script.js
@@ -0,0 +1,40 @@
+const filmes = [
+    {
+        titulo: "Backroms",
+        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/a1pgQeEge47xrS4jGlDwIHiieVK.jpg",
+        descrição:"Um não lugar",
+
+ },
+    {
+        titulo: "spider-noir",
+        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/rfrzXSaiuvRMLo8PRQds0TfFrZC.jpg",
+        descrição:"miranha gotico",
+
+ },
+    {
+        titulo: "O mandaloriano",
+        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/dNwaS0tnwgQRaQFPY5MbGxdmYXr.jpg",
+        descrição:"e grogu",
+
+ },
+    {
+        titulo: "mortal kombat",
+        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/iILiJSRViTEcF23MHhGCbVm3mfW.jpg",
+        descrição:"2",
+ },
+    {
+  
+        titulo: "John Carter",
+        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/ujB1v2jvEjq7euYPkx3zUgFBCbb.jpg",
+        descrição:"Entre dois mundos",
+ },
+    {
+   
+        titulo: "",
+        imagen:"",
+        descrição:"",
+
+    },
+]
+
+const listaFilme = document.getElement
