07300df91a7fc07d56da535f2125e265cecac842
michel luis  pioli <domingos.michel@escola.pr.gov.br>
2026-06-02 10:18:53 -0300

    Add files via upload

--- catalagofilmes/script/script.js
+++ catalagofilmes/script/script.js
@@ -1,40 +1,62 @@
 const filmes = [
     {
         titulo: "Backroms",
-        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/a1pgQeEge47xrS4jGlDwIHiieVK.jpg",
-        descrição:"Um não lugar",
+       imagem:"https://www.themoviedb.org/t/p/w600_and_h900_face/a1pgQeEge47xrS4jGlDwIHiieVK.jpg",
+        descricao:"Um não lugar",
 
  },
     {
         titulo: "spider-noir",
-        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/rfrzXSaiuvRMLo8PRQds0TfFrZC.jpg",
-        descrição:"miranha gotico",
+        imagem:"https://www.themoviedb.org/t/p/w600_and_h900_face/rfrzXSaiuvRMLo8PRQds0TfFrZC.jpg",
+        descricao:"miranha gotico",
 
  },
     {
         titulo: "O mandaloriano",
-        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/dNwaS0tnwgQRaQFPY5MbGxdmYXr.jpg",
-        descrição:"e grogu",
+       imagem:"https://www.themoviedb.org/t/p/w600_and_h900_face/dNwaS0tnwgQRaQFPY5MbGxdmYXr.jpg",
+        descricao:"e grogu",
 
  },
     {
         titulo: "mortal kombat",
-        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/iILiJSRViTEcF23MHhGCbVm3mfW.jpg",
-        descrição:"2",
+        imagem:"https://www.themoviedb.org/t/p/w600_and_h900_face/iILiJSRViTEcF23MHhGCbVm3mfW.jpg",
+         descricao:"2",
  },
     {
   
         titulo: "John Carter",
-        imagen:"https://www.themoviedb.org/t/p/w600_and_h900_face/ujB1v2jvEjq7euYPkx3zUgFBCbb.jpg",
-        descrição:"Entre dois mundos",
- },
-    {
+        imagem:"https://www.themoviedb.org/t/p/w600_and_h900_face/ujB1v2jvEjq7euYPkx3zUgFBCbb.jpg",
+        descricao:"Entre dois mundos",
    
-        titulo: "",
-        imagen:"",
-        descrição:"",
-
     },
 ]
 
-const listaFilme = document.getElement
+const listaFilmes = document.getElementById
+("listaFilmes");
+function mostrarFilmes(lista){
+    listaFilmes.innerHTML = ""
+    lista.forEach(filme => {
+        listaFilmes.innerHTML += `
+            <div class="col-md-4">
+                <div class="card card-filme h-100">
+                    <img src="${filme.imagem}" class="card-img-top">
+                        <div class="card-body">
+                         <h3>${filme.titulo}</h3>
+                         <p>${filme.descricao}</p>
+                         <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes</button>
+                        </div>
+                </div>
+            </div> 
+        `
+    });
+}
+
+mostrarFilmes(filmes)
+
+function verDetalhes(titulo) {
+    Swal.fire({
+        title: titulo,
+        text: "Mais informções do filme",
+        icon: "info"
+    })
+}
